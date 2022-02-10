448e1554fdfa588ce6e1112ef7a39157bf780f3e
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2022-02-10 12:51:36 +0900

    Update auto_thread.py
    delete AutoThread class and add SelectQuestionCategory class for interaction buttons

--- src/cogs/auto_thread.py
+++ src/cogs/auto_thread.py
@@ -1,21 +1,82 @@
 import discord
 from discord.ext import commands, tasks
+from discord import ui, ButtonStyle, Embed
+# from discord.ext import ui
+# from discord.ext.ui import Message, Button
+
+categories = {
+    "errors_and_bugs": "エラーやバグ",
+    "showing_and_technical": "表現系・技術系",
+    "tips": "豆知識",
+    "programming": "プログラミング",
+    "other": "その他"
+}
 
 
-class AutoThread(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.pages = [
-            discord.Embed(title="質問を投稿するには、以下のコマンドを使用してください。",
-                          description="", color=0x00ff00),
-        ]
+class SelectQuestionCategory(ui.View):
 
-    @property
-    def channel_id(self) -> int:
+    def __init__(self, user):
         """
-          Get the channel ID from the config file
+        Initialize
         """
-        raise NotImplementedError
+        super().__init__()
+        self.selected_category = None
+        self.user = user
+
+    @ui.button(label=categories["errors_and_bugs"])
+    async def ask_about_errors_button(self, button: ui.Button, interaction: discord.Interaction):
+        """
+        Set the selected category to Error and Bugs
+        """
+        if interaction.user == self.user:
+            self.selected_category = "errors_and_bugs"
+            self.stop()
+
+    @ui.button(label=categories["showing_and_technical"])
+    async def ask_about_showing_button(self, button: ui.Button, interaction: discord.Interaction):
+        """
+        Set the selected category to Showing and Technical
+        """
+        if interaction.user == self.user:
+            self.selected_category = "showing_and_technical"
+            self.stop()
+
+    @ui.button(label=categories["tips"])
+    async def ask_about_tips_button(self, button: ui.Button, interaction: discord.Interaction):
+        """
+        Set the selected category to Tips
+        """
+        if interaction.user == self.user:
+            self.selected_category = "tips"
+            self.stop()
+
+    @ui.button(label=categories["programming"])
+    async def ask_about_programming_button(self, button: ui.Button, interaction: discord.Interaction):
+        """
+        Set the selected category to Programming
+        """
+        if interaction.user == self.user:
+            self.selected_category = "programming"
+            self.stop()
+
+    @ui.button(label=categories["other"])
+    async def ask_about_others_button(self, button: ui.Button, interaction: discord.Interaction):
+        """
+        Set the selected category to Others
+        """
+        if interaction.user == self.user:
+            self.selected_category = "others"
+            self.stop()
+
+    @ui.button(label="キャンセル", style=ButtonStyle.red)
+    async def cancelling(self, button: ui.Button, interaction: discord.Interaction):
+        """
+        Cancel the selection
+        """
+        if interaction.user == self.user:
+            self.stop()
+
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
